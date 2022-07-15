96bd7157374712e709ea34428cf6583fa83527fd
Ana Cunha <dacunhanajulia@gmail.com>
2022-07-15 11:27:21 -0300

    toString adicionado

--- ContaBancaria.java
+++ ContaBancaria.java
@@ -4,67 +4,64 @@ public abstract class ContaBancaria {
 	private String nomeCliente;
 	private String numConta;
 	private double saldo;
-	
-	public ContaBancaria() {}
-	
+
+	public ContaBancaria() {
+	}
+
 	public ContaBancaria(String nomeCliente, String numConta, double saldo) {
 		this.nomeCliente = nomeCliente;
 		this.numConta = numConta;
 		this.saldo = saldo;
 	}
-	
+
 	public ContaBancaria(String nomeCliente, String numConta) {
 		this.nomeCliente = nomeCliente;
 		this.numConta = numConta;
 	}
-	
+
 	public String getNomeCliente() {
 		return nomeCliente;
 	}
-	
+
 	public void setNomeCliente(String nomeCliente) {
 		this.nomeCliente = nomeCliente;
 	}
-	
+
 	public String getNumConta() {
 		return numConta;
 	}
-	
+
 	public void setNumConta(String numConta) {
 		this.numConta = numConta;
 	}
-	
+
 	public double getSaldo() {
 		return saldo;
 	}
-	
+
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
 
-	public boolean sacar (double valor) {
+	public boolean sacar(double valor) {
 		if (this.saldo != 0 && valor <= this.saldo) {
 			this.saldo -= valor;
 			return true;
-		} else if (valor > this.saldo){
+		} else {
 			return false;
-		} 
-		
+		}
+
 		return false;
 	}
-	
-	public void depositar (double valor) {
+
+	public void depositar(double valor) {
 		this.saldo += valor;
 	}
-	
-	public void mostrarInfo() {
-		System.out.println("---------------------------");
-		System.out.println("Nome: " +this.nomeCliente);
-		System.out.println("Numero da conta: " +this.numConta);
-		System.out.println("Saldo: " +this.saldo);
-		
+
+	@Override
+	public String toString() {
+		return "\n---------------------------" + "\nNome: " + nomeCliente + "" + "\nNumero da conta: " + numConta + ""
+				+ "\nSaldo: " + saldo + "\n";
 	}
-	
-	public void mostrarLimite() {
-	}
+
 }
