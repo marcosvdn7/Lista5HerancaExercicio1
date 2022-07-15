9e2c138836d1fd0d149d10ae6d293df0f63a02f0
marcosvdn7 <104005416+marcosvdn7@users.noreply.github.com>
2022-07-15 09:14:54 -0300

    Create ClasseTeste2.java

---
+++ ClasseTeste2.java
@@ -0,0 +1,25 @@
+package Lista5HerancaExercicio1.Lista5HerancaExercicio1;
+
+import java.util.Scanner;
+
+public class ClasseTeste2 {
+
+	public static void main(String[] args) {
+		Scanner read = new Scanner(System.in);
+		
+		int opcao = 0;
+		
+		while (opcao != 4) {
+			System.out.println("-------------Sistema Bancário-------------");
+			System.out.println("1 - Abrir conta poupança.");
+			System.out.println("2 - Abrir conta especial.");
+			System.out.println("3 - Acessar conta existente.");
+			System.out.println("4 - Encerrar.");
+			
+			
+			
+		}
+
+	}
+
+}
