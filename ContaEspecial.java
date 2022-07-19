5d7b3fbe40c2c382be9f2be3e7ac89891d391822
Ana Cunha <dacunhanajulia@gmail.com>
2022-07-18 22:58:00 -0300

    retorno dos metodos modificados, pequena modificação na logica e adição do toString()

--- ContaEspecial.java
+++ ContaEspecial.java
@@ -1,3 +1,13 @@
+/**
+ * Conta Especial
+ * @date jul/2022
+ * @authors Ana Júlia da Cunha - Entra21
+ * 			Gabriel Roger da Cunha - Entra21
+ * 			Marcos Vinicius do Nascimento - Entra21
+ * 	
+ * Lista 5: Exercicio 1.
+ */
+
 package Lista5HerancaExercicio1;
 
 public class ContaEspecial extends ContaBancaria {
@@ -42,10 +52,10 @@ public class ContaEspecial extends ContaBancaria {
 	/**
 	 * Construtor completo da classe ContaEspecial.
 	 * 
-	 * @param nomeCliente nome do cliente.
-	 * @param numConta    numero da conta em <code>String</code>.
-	 * @param saldo       da conta em <code>double</code>.
-	 * @param limiteMaximo      do cheque especial em <code>double</code>.
+	 * @param nomeCliente  nome do cliente.
+	 * @param numConta     numero da conta em <code>String</code>.
+	 * @param saldo        da conta em <code>double</code>.
+	 * @param limiteMaximo do cheque especial em <code>double</code>.
 	 */
 	public ContaEspecial(String nomeCliente, String numConta, double saldo, double limiteMaximo) {
 		super(nomeCliente, numConta, saldo);
@@ -65,16 +75,16 @@ public class ContaEspecial extends ContaBancaria {
 
 	/**
 	 * Define o limite atual do cheque especial da conta.
-	 *  
+	 * 
 	 * @param limite atual a ser definido em <code>double</code>.
 	 */
 	public void setLimiteAtual(double limiteAtual) {
 		this.limiteAtual = limiteAtual;
 	}
-	
+
 	/**
 	 * Define o limite maximo do cheque especial da conta.
-	 *  
+	 * 
 	 * @param limite maximo a ser definido em <code>double</code>.
 	 */
 	public double getLimiteMaximo() {
@@ -83,58 +93,99 @@ public class ContaEspecial extends ContaBancaria {
 
 	/**
 	 * Define o limite maximo do cheque especial da conta.
-	 *  
+	 * 
 	 * @param limite maximo a ser definido em <code>double</code>.
 	 */
 	public void setLimiteMaximo(double limiteMaximo) {
 		this.limiteMaximo = limiteMaximo;
 	}
-	
+
 	// METODOS
 
 	@Override
-	public boolean sacar(double valor) {
-		double resto = 0;
-		if (valor > super.getSaldo() + this.limiteAtual && super.getSaldo() >= 0) {
-			return false;
-		} else if (valor <= super.getSaldo()) {
-			super.setSaldo(super.getSaldo() - valor);
-			return true;
-		} else if (valor > super.getSaldo() && this.limiteAtual <= 0) {
-			return false;
-		} else if (valor > super.getSaldo() && this.limiteAtual > 0) {
-			if (super.getSaldo() > 0) {
-				resto = valor - super.getSaldo();
-				super.setSaldo(super.getSaldo() - valor);
-				this.limiteAtual -= resto;
-				return true;
-			} else if (super.getSaldo() <= 0 && valor > this.limiteAtual) {
-				return false;
-			}
-		}
-		if (super.getSaldo() <= 0 && valor <= this.limiteAtual) {
-			this.limiteAtual -= valor;
-			super.setSaldo(super.getSaldo() - valor);
-			return true;
+	public int sacar(double valor) {
+
+		if (valor <= 0) {
+			return -1; // VALOR NULO OU NEGATIVO
 		}
 
-		return false;
+		// SE O VALOR FOR MENOR OU IGUAL AO SALDO
+		else if (valor <= super.getSaldo()) {
+			super.setSaldo(super.getSaldo() - valor);
+			return 0; // SUCESSO
+		}
+
+		// SE O VALOR FOR MAIOR QUE O SALDO SOMADO AO LIMITE ATUAL E O SALDO FOR MAIOR
+		// OU IGUAL A ZERO
+		else if (valor > (super.getSaldo() + this.limiteAtual) && super.getSaldo() >= 0) {
+			return -2; // INSUFICIENCIA DE SALDO E LIMITE
+		}
+
+		// SE O VALOR FOR MAIOR QUE O SALDO E NAO HOUVER LIMITE
+		else if (valor > super.getSaldo() && this.limiteAtual <= 0) {
+			return -2; // INSUFICIENCIA DE SALDO E LIMITE
+		}
+
+		// SE O VALOR FOR MAIOR QUE O SALDO E HOUVER LIMITE
+		else if (valor > super.getSaldo() && this.limiteAtual > 0) {
+
+			// SE O SALDO FOR MENOR OU IGUAL A ZERO
+			if (super.getSaldo() <= 0) {
+				this.limiteAtual -= valor; // SACA-SE O VALOR DO LIMITE
+				super.setSaldo(super.getSaldo() - valor); // E DESCONTA-SE NO SALDO
+				return 1; // SUCESSO, MAS DESCONTADO DO LIMITE
+			}
+			// SE HOUVER SALDO
+			else {
+				double insuficiente = valor - super.getSaldo(); // PEGA O VALOR INSUFICIENTE PARA SAQUE
+
+				// SE O INSUFICIENTE FOR MAIOR QUE O LIMITE
+				if (insuficiente > this.limiteAtual) {
+					return -2; // INSUFICIENCIA DE SALDO E LIMITE
+				} else {
+					super.setSaldo(super.getSaldo() - valor); // SACA-SE O VALOR DO SALDO
+					this.limiteAtual -= insuficiente; // E DESCONTA-SE O INSUFICIENTE DO LIMITE ATUAL
+					return 1; // SUCESSO, MAS DESCONTADO DO LIMITE
+				}
+			}
+
+		}
+
+		return -3; // ERRO INESPERADO
 	}
 
-	public void depositar(double valor) {
-		if (this.limiteAtual < this.limiteMaximo && super.getSaldo() <= 0) {
-			this.limiteAtual += valor;
-			super.setSaldo(super.getSaldo() + valor);
-			if (this.limiteAtual > limiteMaximo) {
-				this.limiteAtual = limiteMaximo;
+	@Override
+	public int depositar(double valor) {
+
+		if (valor <= 0) {
+			return -1; // VALOR NULO OU NEGATIVO
+
+		} else {
+			// SE O LIMITE ATUAL FOR MENOR QUE O LIMITE MAXIMO E O SALDO FOR MENOR OU IGUAL
+			// A ZERO, O VALOR DEPOSITADO AUMENTA O LIMITE
+			if (this.limiteAtual < this.limiteMaximo && super.getSaldo() <= 0) {
+				this.limiteAtual += valor;
+
+				// SE O LIMITE ULTRAPASSAR O LIMITE MAX, ELE RECEBE O VALOR DO LIMITE MAXIMO
+				if (this.limiteAtual > limiteMaximo) {
+					this.limiteAtual = limiteMaximo;
+				}
+
+				// E DEPOSITA O VALOR NA CONTA
+				super.depositar(valor);
+
+				return 0; // SUCESSO
+
+			} else {
+				return -3; // ERRO INESPERADO
 			}
-		} else if (this.limiteAtual == limiteMaximo) {
-			super.setSaldo(super.getSaldo() + valor);
 		}
 	}
 
-	public void mostrarLimite() {
-		System.out.println(this.limiteAtual);
+	@Override
+	public String toString() {
+		return super.toString() + "Tipo de conta: Especial" + "\nLimite atual: " + limiteAtual + "\nLimite Maximo: "
+				+ limiteMaximo + "\n---------------------------";
 	}
 
 }
